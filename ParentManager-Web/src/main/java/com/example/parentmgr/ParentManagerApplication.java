b0759bf276001b811b27b00c0b711c5e26101690
shyamala <shyamala@example.com0>
2020-03-08 02:22:18 +0530

    Changes in maven

--- ParentManager-Web/src/main/java/com/example/parentmgr/ParentManagerApplication.java
+++ ParentManager-Web/src/main/java/com/example/parentmgr/ParentManagerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -18,7 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableJpaAuditing
 @ComponentScan(basePackages = "com.example.parentmgr")
-public class ParentManagerApplication extends SpringBootServletInitializer{
+public class ParentManagerApplication{
 	
 	private final static  Logger logger = LoggerFactory.getLogger(ParentManagerApplication.class);
 	
