f12a39877a7f21e1697af3490880adbd66e5d307
shyamala <shyamala@example.com0>
2020-03-08 03:16:55 +0530

    Edit and View User Details

--- ParentManager-Web/src/main/java/com/example/parentmgr/controller/TaskController.java
+++ ParentManager-Web/src/main/java/com/example/parentmgr/controller/TaskController.java
@@ -7,7 +7,10 @@ import com.example.parentmgr.service.UserManagerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -25,8 +28,34 @@ public class TaskController {
     
 	@PostMapping(path="/add/user")
 	public Users addUser(@Valid @RequestBody Users user) {
-		logger.info("Add a New User for test");
+		logger.info("Add a New User");
 		return usermgrService.addUser(user);
+	}
+	
+	@GetMapping(path="/all/users", produces = "application/json")
+	public ResponseEntity<List<Users>>  getAllUsers() {
+		logger.info("Listed all User details"+usermgrService.getAllUsers());
+		List<Users> userslist = usermgrService.getAllUsers();
+		
+		return ResponseEntity.ok().body(userslist);
+	}
+	
+	@GetMapping(path="/get/user/{id}", produces = "application/json")
+	public ResponseEntity<Users> getUser(@PathVariable(value = "id") Long user_Id) {
+		logger.info("Retrieve the existing User By Id");
+		
+		Users user = usermgrService.getUserByID(user_Id);
+
+		return ResponseEntity.ok().body(user);
+	}
+
+	@PutMapping(path="/users/{id}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Users> updateUser(@PathVariable(value = "id") Long user_Id, @Valid @RequestBody Users userDetails) {
+		logger.info("Updated the existing User");
+
+		Users user =  usermgrService.updateuser(user_Id, userDetails);
+		
+		return ResponseEntity.ok(user);
 	}
 
 }
--- ParentManager-Web/src/main/java/com/example/parentmgr/service/UserManagerService.java
+++ ParentManager-Web/src/main/java/com/example/parentmgr/service/UserManagerService.java
@@ -1,14 +1,20 @@
 package com.example.parentmgr.service;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.parentmgr.exception.EntityExistsException;
+import com.example.parentmgr.exception.ResourceNotFoundException;
 import com.example.parentmgr.model.Users;
 import com.example.parentmgr.repository.UsersRepository;
+
+
 
 @Service
 public class UserManagerService {
@@ -29,6 +35,46 @@ public class UserManagerService {
 		}
 
 	}
+	
+	public List<Users> getAllUsers() {
+
+		logger.debug("Service to get list of Users");
+		return usersRepository.findAll();
+	}
+
+	public Users updateuser(Long userId, Users userDetails) {
+
+		logger.debug("Service to create a Update User");
+		
+		Users user = usersRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Users", "user_ID", userId));
+		
+		if (usersRepository.findByemployeeID(userDetails.getEmployeeID()) == null || user.getEmployeeID().equalsIgnoreCase(userDetails.getEmployeeID()) ) {			
+			user.setFirstName(userDetails.getFirstName());
+			user.setLastName(userDetails.getLastName());
+			user.setEmployeeID(userDetails.getEmployeeID());
+
+			logger.debug("Service to update the existing user");
+			
+		} else {
+			throw new EntityExistsException("Users", "employee_id", userDetails.getEmployeeID());
+		}	
+		return usersRepository.save(user);
+	}
+
+	public Users getUserByID(Long userId) throws ResourceNotFoundException {
+
+		logger.debug("Service to get tasks by id");
+
+		Optional<Users> user = usersRepository.findById(userId);
+
+		if (user.isPresent()) {
+			return user.get();
+		} else {
+			throw new ResourceNotFoundException("Users", "user_ID", userId);
+		}
+	}
+	
 
 
 }
